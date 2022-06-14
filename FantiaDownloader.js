19720d49decffbc5023b49b029d8191c21ac6690
Yeh Ting-Yu <j0819902@gmail.com>
2022-06-14 20:19:50 +0800

    3.0.1

--- FantiaDownloader.js
+++ FantiaDownloader.js
@@ -4,7 +4,7 @@
 // @name:en      Fantia downloader
 // @name:ja      Fantia downloader
 // @namespace    http://tampermonkey.net/
-// @version      3.0.0
+// @version      3.0.1
 // @description  Download your Fantia rewards more easily!
 // @description:en  Download your Fantia rewards more easily!
 // @description:ja  Download your Fantia rewards more easily!
